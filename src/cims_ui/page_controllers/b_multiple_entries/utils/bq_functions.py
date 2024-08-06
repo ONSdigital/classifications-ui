0ca028139804b8d3439b21df81108c77d419f7a7
wrighja <148550943+wrighja@users.noreply.github.com>
2024-08-06 15:52:16 +0000

    testing - remove try/except from bq functions to reveal traceback error in the load from file method

--- src/cims_ui/page_controllers/b_multiple_entries/utils/bq_functions.py
+++ src/cims_ui/page_controllers/b_multiple_entries/utils/bq_functions.py
@@ -16,24 +16,24 @@ def load_csv_into_bigquery(file):
     bq_dataset = app.config.get('BQ_DATASET')
     bq_table = app.config.get('BQ_TABLE')
     table_id = f'{project_id}.{bq_dataset}.{bq_table}.{start}' 
-    try:
-        with open(file, 'rb') as source_file:
-            job = client.load_table_from_file(
-                source_file,
-                table_id, 
-                job_config=bigquery.LoadJobConfig(
-                    source_format=bigquery.SourceFormat.CSV,
-                    skip_leading_rows=1,
-                    autodetect=True,
-                )
+    # try:
+    with open(file, 'rb') as source_file:
+        job = client.load_table_from_file(
+            source_file,
+            table_id, 
+            job_config=bigquery.LoadJobConfig(
+                source_format=bigquery.SourceFormat.CSV,
+                skip_leading_rows=1,
+                autodetect=True,
             )
-            job.result()  # Waits for the job to complete.
-        logging.info(f'Loaded {job.output_rows} rows into {table_id}')
-        logging.info(f'Time taken: {time.time() - start} seconds')
+        )
+        job.result()  # Waits for the job to complete.
+    logging.info(f'Loaded {job.output_rows} rows into {table_id}')
+    logging.info(f'Time taken: {time.time() - start} seconds')
         
-    except FileNotFoundError:
-        logging.error(f'Error: File not found: {file}')
-    except Exception as e:
-        logging.error(f"Error loading data into BigQuery: {e}")
+    # except FileNotFoundError:
+    #     logging.error(f'Error: File not found: {file}')
+    # except Exception as e:
+    #     logging.error(f"Error loading data into BigQuery: {e}")
     
     return table_id
